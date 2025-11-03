994ca5cb420b600c18c6201127edb321434f240a
andr4arthur <andrearthurmendonca@gmail.com>
2025-11-03 14:12:41 -0300

    3/11

--- array.php
+++ array.php
@@ -18,13 +18,18 @@
 
     function dataAtual(): string
     {
-        $daiMes = date("d");
+        $diaMes = date("d");
         $diaSemana = date("w");
         $mes = date("m");
         $ano = date("Y");
 
-        $nomeDosMeses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho','julho', 'agosto', 'setembro', 'outubro', 'novembro' , 'dezembro'];
+        $nomeDosMeses = ['janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
 
-        $nomeDiaSemana = [ 'domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira' , 'sábado'];
-        $dataAtual =  $diaMes. '.' . $nomeDiaSemana, $ano);
+        $nomeDiasDaSemana = ['domingo' , 'segunda-feira' , 'terça-feira' , 'quarta-feira' , 'quinta-feira' , 'sexta-feira' , 'sabado'];
+
+        $dataFormatada = $nomeDiasDaSemana[$diaSemana] . ', ' . $diaMes . ' de ' . $nomeDosMeses[$mes - 1] . ' de ' . $ano;
+
+        echo 'Dia do mês: ' . $diaMes . '<br>' . 'Dia Semana: ' . $diaSemana . '<br>' . 'Mês: ' . $mes . '<br>' . 'Ano: ' . $ano . '<br>';
+        return $dataFormatada;
     }
+echo dataAtual();
